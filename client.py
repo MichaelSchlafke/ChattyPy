9d28ccf677f89259a3e7413985966d6f0477e3a0
MichaelSchlafke <82033132+MichaelSchlafke@users.noreply.github.com>
2021-06-08 17:25:43 +0200

    made recieving into seperate thread
    fixes simoultaneous sending and recieving

--- client.py
+++ client.py
@@ -8,6 +8,7 @@ Created on Tue Jun  8 14:03:43 2021
 
 
 import socket 
+import _thread
 
 alias = "client1"
 
@@ -17,34 +18,29 @@ port = 12020#<INTEGER>
 sock = socket.socket()
 sock.connect((host, port))
 
+def recieve_msg():
+    while True:
+        msg = sock.recv(1024).decode()+'\n'
+        print(msg)
+
+_thread.start_new_thread(recieve_msg)
+
 while True: #Sendeschleife
 
-    msg = sock.recv(1024).decode()+'\n'
-
-    print(msg)
-
     #Eingabe der Sendedaten mit input()
-    while True:
-        #print("enter message: ")
-        msg_clt = input()
-        if not msg_clt:
-            msg = sock.recv(1024).decode()+'\n'
-            print(msg)
-            break           
-        else:
-            print("\n")
-            msg_clt = alias + ": \n" + msg_clt
-            msg_clt_byt = bytes(msg_clt, 'utf-8')
-        
-
+    #print("enter message: ")
+    msg_clt = input()
+    if not msg_clt:
+        #msg = sock.recv(1024).decode()+'\n'
+        #print(msg)
+        break           
+    else:
+        print("\n")
+        msg_clt = alias + ": \n" + msg_clt
+        msg_clt_byt = bytes(msg_clt, 'utf-8')
         #Senden der eingegebenen Daten
-
         sock.send(msg_clt_byt)
-
     ##Warten auf Antwort des Servers
-
-        msg = sock.recv(1024).decode()+'\n'
-
+    #msg = sock.recv(1024).decode()+'\n'
     ##Ausgabe der Server Antwort
-
-        print(msg)
+    #print(msg)
