f85632ccedd543ead1b46db6c6ec80ee774d3b1d
MichaelSchlafke <82033132+MichaelSchlafke@users.noreply.github.com>
2021-06-09 19:01:38 +0200

    Adds auto IP + port

--- client.py
+++ client.py
@@ -13,8 +13,8 @@ import _thread
 alias = "client1"
 
 # socket anlegen
-host = '129.217.162.178'
-port = 12020#<INTEGER>
+host = '129.217.162.205'
+port = 52191#<INTEGER>
 sock = socket.socket()
 sock.connect((host, port))
 
@@ -28,11 +28,8 @@ _thread.start_new_thread(recieve_msg)
 while True: #Sendeschleife
 
     #Eingabe der Sendedaten mit input()
-    #print("enter message: ")
     msg_clt = input()
     if not msg_clt:
-        #msg = sock.recv(1024).decode()+'\n'
-        #print(msg)
         break           
     else:
         print("\n")
@@ -40,7 +37,3 @@ while True: #Sendeschleife
         msg_clt_byt = bytes(msg_clt, 'utf-8')
         #Senden der eingegebenen Daten
         sock.send(msg_clt_byt)
-    ##Warten auf Antwort des Servers
-    #msg = sock.recv(1024).decode()+'\n'
-    ##Ausgabe der Server Antwort
-    #print(msg)
