5666bd4b7f9b6d1c80119a7af6679cf2eceef712
MichaelSchlafke <82033132+MichaelSchlafke@users.noreply.github.com>
2021-06-10 14:07:15 +0200

    Adds serverside aliases and some exceptionhandling

--- client.py
+++ client.py
@@ -9,19 +9,79 @@ Created on Tue Jun  8 14:03:43 2021
 
 import socket 
 import _thread
+import time
+import os
+import sys
 
-alias = "client1"
+#alias
+print("input alias:")
+alias = input()
+if not alias:
+    alias = "anon"
+
+connected = False
+
+#settings
+
+localhost = True #is the host local
+
+manual_port = True #aks for port?
 
 # socket anlegen
-host = '129.217.162.205'
-port = 52191#<INTEGER>
+#port
+if manual_port:
+    print("enter server port:")
+    port = int(input())
+else:
+    port = 12002#<INTEGER>
+#ip
+if localhost:
+    host = socket.gethostbyname(socket.gethostname())
+else:
+    host = '129.217.162.205'
+
+def reconnect():
+    connected = False
+    while not connected:   
+        try:  
+            print("re-enter port:")
+            port_in = input()
+            if port_in:
+                port = port_in
+            print("trying to reconnect at: " + host + ":" + str(port))
+            sock.connect( ( host, int(port) ) )  
+            connected = True  
+            print( "re-connection successful" )
+            break
+        except socket.error:  
+            print("attempt not successfull")
+            time.sleep( 2 )  
+
+#attempt establishing connection
+#try:
 sock = socket.socket()
+print("connecting to " + str(host) + ":" + str(port))
 sock.connect((host, port))
+#finally:
+    #fix this!!!
+    #print("connection failed! re-enter port:")
+    #reconnect()
+    #os.execl(sys.executable, sys.executable, *sys.argv)
+
+#info nachricht
+msg_sttgs = "/alias=" + alias
+msg_sttgs_byt = bytes(msg_sttgs, 'utf-8')
+sock.send(msg_sttgs_byt)
 
 def recieve_msg():
-    while True:
-        msg = sock.recv(1024).decode()+'\n'
-        print(msg)
+    try:
+        while True:
+            msg = sock.recv(1024).decode()+'\n'
+            print(msg)
+    finally:
+        print("connection lost")
+        _thread.start_new_thread(reconnect)
+
 
 _thread.start_new_thread(recieve_msg)
 
@@ -33,7 +93,7 @@ while True: #Sendeschleife
         break           
     else:
         print("\n")
-        msg_clt = alias + ": \n" + msg_clt
+        #msg_clt = alias + ": \n" + msg_clt
         msg_clt_byt = bytes(msg_clt, 'utf-8')
         #Senden der eingegebenen Daten
         sock.send(msg_clt_byt)
--- server.py
+++ server.py
@@ -15,13 +15,20 @@ import _thread
 # Neuen Thread starten, recherchieren wie die Funktion runktioniert.
 
 #settings
+manual_port_overwite = False
+port = 64303
+
 manual_set_ip = False
 
 #socket anlegen
 sock = socket.socket()
 
 #IP setting
-if manual_set_ip:
+if manual_port_overwite:
+    host_auto = socket.gethostbyname(socket.gethostname())
+    sock.bind((host_auto, port))
+    print("hosting at: " + str(host_auto) + ":" + str(port))
+elif manual_set_ip:
     #user input defined
     print("Input host IP adress:")
     host_manual = input()
@@ -44,38 +51,66 @@ else:
 sock.listen(100)
 
 clients = set()
+identities = {}
 
-print("socked created")
+
+print("socket created")
 
 def handle_client(client_socket, adress):
     #load history
-    with open('history.txt','r') as text_file:
-        history = text_file.read()
-        history_b = bytes(history, 'utf-8')
-        client_socket.sendall(history_b)
+    alias = "anon"
+    try:
+        with open('history.txt','r') as text_file:
+            history = text_file.read()
+            history_b = bytes(history, 'utf-8')
+            client_socket.sendall(history_b)
 
-        # Warten auf Nachricht der Clientseite
-        while True:
-            msg = client_socket.recv(1024).decode()+'\n'
-            if not msg:
-                            break
-            else:
-                if "@" in msg:
-                    print("command detected!")
-                    #add alliases
+            # Warten auf Nachricht der Clientseite
+            while True:
+                msg = client_socket.recv(1024).decode()+'\n'
+                if not msg:
+                    break
                 else:
-                    # Ausgabe der Clientnachricht
-                    now = datetime.now()
-                    current_time = now.strftime("%H:%M:%S")
-                    msg = (current_time + ", " + msg)
-                    print(msg) 
-                    with open('history.txt','a') as text_file:
-                        text_file.write(msg + '\n')
-                    # Weiterleitung der Nachricht an alle anderen clients
-                    msg = bytes(msg, 'utf-8')
-                    for c in clients:
-                        c.sendall(msg)
-                        #print("send to " + str(c)) #debugging
+                    if "/" in msg:
+                        if "/alias=" in msg:
+                            alias = msg[7:]
+                            alias = alias.replace('\n', '').replace('\r', '')
+                            #connect alias to ip
+                            identities[alias] = (client_socket)
+                            msg_comm_resp = "[Name set to " + alias + "]"
+                            msg_conn = "A wild " + alias + " appears!\t"
+                            for c in clients:
+                                c.sendall(bytes(msg_conn, 'utf-8'))
+                            print("current users: \n" + str(identities))
+                        client_socket.sendall(bytes(msg_comm_resp, 'utf-8'))
+                    elif "@" in msg:
+                        print("command detected!")
+                        #add alliases
+                    else:
+                        # Ausgabe der Clientnachricht
+                        now = datetime.now()
+                        current_time = now.strftime("%H:%M:%S")
+                        msg = (alias + ", " + current_time + ":\n" + msg)
+                        print(msg) 
+                        with open('history.txt','a') as text_file:
+                            text_file.write(msg + '\n')
+                        # Weiterleitung der Nachricht an alle anderen clients
+                        msg = bytes(msg, 'utf-8')
+                        for c in clients:
+                            c.sendall(msg)
+                            #print("send to " + str(c)) #debugging
+    #except socket.error:
+        #print("logging error")
+        #with open('error_log.txt','a') as text_file:
+            #text_file.write("???" + '\n')
+    except socket.error:
+        print("closing socket: " + str(client_socket))
+        msg = (alias + " disconnected!")
+        clients.remove(client_socket)
+        client_socket.close()
+        print(msg)
+        for c in clients:
+                c.sendall(bytes(msg, 'utf-8'))
                     
                 
 
@@ -90,7 +125,10 @@ while True:
     conn, addr_new = sock.accept()
     clients.add(conn)
     addr.append(addr_new)
-    print(addr[num_conn], ' just joind the chat')
+    msg_conn = str(addr[num_conn]) + ' just joind the chat'
+    print(msg_conn)
+    #for c in clients:
+        #c.sendall(bytes(msg_conn, 'utf-8'))
     num_conn += 1
 
     conn.send(b"Welcome to the Server.\n")
